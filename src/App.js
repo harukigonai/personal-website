10365299cebcb2d191420b43ae5e8cc2d88d0d0d
harukigonai <hg2541@columbia.edu>
2021-08-20 23:39:48 -0400

    Made home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import ContactMe from "./pages/ContactMe";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 import "./App.css";
+import "./fonts.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/personal-website">
+      <div id="navbar">
+        <Link id="harukiGonai" to="/">
+          harukiGonai
+        </Link>
+        <div id="navbarLinks">
+          <Link className="navbarLink" to="/">
+            Home
+          </Link>
+          <Link className="navbarLink" to="/">
+            Projects
+          </Link>
+          <Link className="navbarLink" to="/">
+            Contact Me
+          </Link>
+        </div>
+      </div>
+
+      <div id="contentWrapper">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contactMe">
+            <ContactMe />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Home() {
+  const homeCategory = (title, description) => (
+    <div className="homeCategory">
+      <h2 className="homeCategoryTitle">{title}</h2>
+      <h3 className="homeCategoryDesc">{description}</h3>
+    </div>
+  );
+
+  return (
+    <>
+      <div id="homeContent">
+        <h1 id="homeHeader">Haruki Gonai</h1>
+        <h3 id="homeSubHeader">Software Engineer</h3>
+        {homeCategory("Columbia University", "CS Major, Class of 2023")}
+        {homeCategory("Sonar Health", "Frontend Engineer")}
+        {homeCategory("Musician", "Double Bassist")}
+      </div>
+      <img id="myPhoto" src={process.env.PUBLIC_URL + "/me.JPG"}></img>
+    </>
+  );
+}
